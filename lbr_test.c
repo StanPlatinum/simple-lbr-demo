7a7b7e8f13dd6941897c73f6254e9c6cea3f93d7
lpjlwj <lpjlwjlpjlwj@126.com>
2019-06-17 15:35:40 -0400

    alpha version

--- lbr_test.c
+++ lbr_test.c
@@ -1,5 +1,9 @@
+
+#include <stdio.h>
+
 #include "msr.h"
 #define IA32_DEBUGCTLMSR_LBR		(1<<0) /* Last Branch Record */
+
 /*Skylake add*/
 #define MSR_LBR_SELECT              0x000001c8
 #define SELECT_CONFIG               0x18D                   //110001101
@@ -10,7 +14,7 @@ void lbr_enable(void *info) {
 	  rdmsrl(MSR_IA32_DEBUGCTLMSR, debugctl);
 	  debugctl |= (IA32_DEBUGCTLMSR_LBR );
 	  wrmsrl(MSR_IA32_DEBUGCTLMSR, debugctl);*/
-	printk("intel lbr enable..\n");
+	//printk("intel lbr enable..\n");
 	unsigned long long select;
 	asm volatile    (
 			"xor %%edx, %%edx;"
@@ -23,6 +27,6 @@ void lbr_enable(void *info) {
 			);
 
 	rdmsrl(MSR_LBR_SELECT,select);
-	printk("SELECT 0x%llx", select);
+	//printk("SELECT 0x%llx", select);
 	wrmsrl(MSR_LBR_SELECT, SELECT_CONFIG);
 }
